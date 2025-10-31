fb06303518407c79a12978b610717489366fb143
mczka <58855170+mczka@users.noreply.github.com>
2025-10-31 14:53:59 +0100

    Update 0100_pointers.c

--- 0100_pointers.c
+++ 0100_pointers.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+ #include<stdio.h>
 
 int main(void) {
 
@@ -6,55 +6,65 @@ int main(void) {
      *    name: num
      *    value: 6
      *    type: int
-     *    location: where the variable i stored
+     *    location: where the variable is stored
      *    address: label for the location
      */
     int num = 6;     /* Integer */
     int * ptr;       /* Pointer to an integer (or integer pointer) */
 
-    /* & : reference operator - returns the address */
-    /* * : dereference operator - returns an alias */
+    printf("   & : reference operator - returns the address\n");
+    printf("   * : dereference operator - returns an alias [value]\n\n");
 
     ptr = &num;    /* ptr holds the address of num */
-    printf("num = %d, address %p, size=%ld\n", 
-            num, (void *) &num, sizeof(num));
-    printf("ptr = %p, address %p, size=%ld, *ptr=%d\n", 
-           (void *) ptr, (void *) &ptr, sizeof(ptr), *ptr);
+    printf("   ptr holds the address of num (value)\n");
+    printf("ADDR: %p  DATA: [num or *ptr = %d, size=%ld]\n", 
+            (void *) &num, num, sizeof(num));
+    printf("ADDR: %p  DATA: [ptr = %p size=%ld] ->  DATA: *ptr=%d\n", 
+           (void *) &ptr, (void *) ptr, sizeof(ptr), *ptr);
 
-    /* *p is an alias of num */
+    /* *ptr is an alias of num */
     *ptr = 20;
-    printf("\nAfter *ptr=20\n");
-    printf("num = %d, address %p, size=%ld\n", 
-            num, (void *) &num, sizeof(num));
-    printf("ptr = %p, address %p, size=%ld, *ptr=%d\n",
-            (void *) ptr, (void *) &ptr, sizeof(ptr), *ptr);
+
+    printf("\n   *ptr is an alias of num (value) -> new value of num *ptr=20\n");
+    printf("ADDR: %p  DATA: [num or *ptr = %d size=%ld]\n", 
+            (void *) &num, num, sizeof(num));
+    printf("ADDR: %p  DATA: [ptr = %p size=%ld] -> DATA: *ptr=%d\n",
+            (void *) &ptr, (void *) ptr, sizeof(ptr), *ptr);
 
     printf("\n");
-    printf("ptr & num: \n");
-    printf("  ptr = %p\n", (void *) ptr);
-    printf("  *ptr = %d\n", *ptr);
-    printf("  num = %d\n", num);
-    printf("  &num = %p\n", (void *) &num);
-    printf("  *(&num) = %d\n", *(&num));
-    printf("  &(*ptr) = %p\n", (void *) &(*ptr));
+    printf("Operator Variable = value: \n");
+    printf("  num\t=\t%d\n", num);
+    printf(" &num\t=\t%p\n", (void *) &num);
+    printf("  ptr\t=\t%p\n", (void *) ptr);
+    printf(" *ptr\t=\t%d\n", *ptr);
+    printf(" &ptr\t=\t%p\n", (void *) &ptr);
+    printf("*(&num)\t=\t%d\n", *(&num));
+    printf("&(*ptr)\t=\t%p\n", (void *) &(*ptr));
   
     return(0);
 }
+
 /*
 clang 0100_pointers.c
-./a.out
-num = 6, address 0x7ffc42044694, size=4
-ptr = 0x7ffc42044694, address 0x7ffc42044688, size=8, *ptr=6
+./a.out 
+   & : reference operator - returns the address
+   * : dereference operator - returns an alias [value]
 
-After *ptr=20
-num = 20, address 0x7ffc42044694, size=4
-ptr = 0x7ffc42044694, address 0x7ffc42044688, size=8, *ptr=20
+   ptr holds the address of num (value)
+ADDR: 0x7ffcd57b8844  DATA: [num or *ptr = 6, size=4]
+ADDR: 0x7ffcd57b8838  DATA: [ptr = 0x7ffcd57b8844 size=8] ->  DATA: *ptr=6
 
-ptr & num: 
-  ptr = 0x7ffc42044694
-  *ptr = 20
-  num = 20
-  &num = 0x7ffc42044694
-  *(&num) = 20
-  &(*ptr) = 0x7ffc42044694
+   *ptr is an alias of num (value) -> new value of num *ptr=20
+ADDR: 0x7ffcd57b8844  DATA: [num or *ptr = 20 size=4]
+ADDR: 0x7ffcd57b8838  DATA: [ptr = 0x7ffcd57b8844 size=8] -> DATA: *ptr=20
+
+Operator Variable = value: 
+  num	=	20
+ &num	=	0x7ffcd57b8844
+  ptr	=	0x7ffcd57b8844
+ *ptr	=	20
+ &ptr	=	0x7ffcd57b8838
+*(&num)	=	20
+&(*ptr)	=	0x7ffcd57b8844
+
 */
